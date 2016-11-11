2311e66230ff2c1cedb1608c2708269c7c13fa1d
ersi <ersi@ersi-PC>
2016-11-11 20:46:23 +0700

    12141425-selanjutnya kita perlu membuat satu variabel yang berisi nilai angka yang harus ditebak oleh user dan kita juga butuh satu variabel yang menampung nilai inputangka tebakan user dan menggunakan objek Scanner

--- test/rizkifaujim/app12141425/tebakangka.java
+++ test/rizkifaujim/app12141425/tebakangka.java
@@ -6,10 +6,17 @@ import java.util.Scanner;
 public class tebakangka {
     public static void main(String[] args){
         
-        int random,tebak,jumah;
+        int random,tebak,jumlah;
         random = (int)(Math.random()*100);
         System.out.println("Tebaklah Angka Antara 1-100");
         Scanner masukan = new Scanner(System.in);
+        jumlah = 0;
+        do{
+            jumlah++;
+            System.out.print("Masukan Tebakan Anda : ");
+            tebak=masukan.nextInt();
+        }
+        
         
     }
     
