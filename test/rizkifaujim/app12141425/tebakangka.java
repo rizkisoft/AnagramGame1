a9499cdbe7be6dd57e4a98b3b6b084d00e7a5d7d
ersi <ersi@ersi-PC>
2016-11-11 20:18:59 +0700

    12141425-daam methode main(),untuk meminta niai inputan dari user pada saat program dijalankan

---
+++ test/rizkifaujim/app12141425/tebakangka.java
@@ -0,0 +1,16 @@
+package rizkifaujim.app12141425;
+
+import java.util.Scanner;
+
+
+public class tebakangka {
+    public static void main(String[] args){
+        
+        int random,tebak,jumah;
+        random = (int)(Math.random()*100);
+        System.out.println("Tebaklah Angka Antara 1-100");
+        Scanner masukan = new Scanner(System.in);
+        
+    }
+    
+}
